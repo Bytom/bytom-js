0608006097fb790db4c80c523711a81584ab7fa2
Zhiting Lin <zlin035@uottawa.ca>
2019-11-28 15:18:43 +0800

    init bytomjs lib.

---
+++ test/input.js
@@ -0,0 +1,96 @@
+'use strict';
+
+let should = require('chai').should();
+let expect = require('chai').expect;
+let _ = require('lodash');
+
+const BufferReader = require('../lib/binary/reader.js')
+let Input = require('../lib/transaction/input.js')
+let BN = require('bn.js')
+
+
+describe('Input', function() {
+
+  let arg = [new Buffer("arguments1"),new Buffer("arguments2")]
+  let issuance = Input.newIssuanceInput(new Buffer("nonce"), new BN(254354), new Buffer("issuanceProgram"), arg, new Buffer("assetDefinition"))
+
+  let iwantHex = [
+    "01",         // asset version
+    "2a",         // serialization length
+    "00",         // issuance type flag
+    "05",         // nonce length
+    "6e6f6e6365", // nonce
+    "a69849e11add96ac7053aad22ba2349a4abf5feb0475a0afcadff4e128be76cf", // assetID
+    "92c30f", // amount
+    "38",     // input witness length
+    "0f",     // asset definition length
+    "6173736574446566696e6974696f6e", // asset definition
+    "01", // vm version
+    "0f", // issuanceProgram length
+    "69737375616e636550726f6772616d", // issuance program
+    "02", // argument array length
+    "0a", // first argument length
+    "617267756d656e747331", // first argument data
+    "0a", // second argument length
+    "617267756d656e747332", // second argument data
+  ].join("")
+
+  let spend = Input.newSpendInput(arg, "fad5195a0c8e3b590b86a3c0a95e7529565888508aecca96e9aeda633002f409", "fe9791d71b67ee62515e08723c061b5ccb952a80d804417c8aeedf7f633c524a",new BN(254354), new BN(3), new Buffer("spendProgram"))
+
+  let swantHex = [
+    "01", // asset version
+      "54", // input commitment length
+      "01", // spend type flag
+      "52", // spend commitment length
+      "fad5195a0c8e3b590b86a3c0a95e7529565888508aecca96e9aeda633002f409", // source id
+      "fe9791d71b67ee62515e08723c061b5ccb952a80d804417c8aeedf7f633c524a", // assetID
+      "92c30f", // amount
+      "03",     // source position
+      "01",     // vm version
+      "0c",     // spend program length
+      "7370656e6450726f6772616d", // spend program
+      "17", // witness length
+      "02", // argument array length
+      "0a", // first argument length
+      "617267756d656e747331", // first argument data
+      "0a", // second argument length
+      "617267756d656e747332", // second argument data
+  ].join("")
+
+  let coinbase = Input.newCoinbaseInput(new Buffer("arbitrary"))
+  let cwantHex = [
+    "01",                 // asset version
+      "0b",                 // input commitment length
+      "02",                 // coinbase type flag
+      "09",                 // arbitrary length
+      "617262697472617279", // arbitrary data
+      "00",                 // witness length
+  ].join("")
+
+
+  it('Issuance: Test convert struct to hex', function() {
+    issuance.writeTo().concat().toString('hex').should.equal(iwantHex)
+  });
+
+  it('Issuance: Test convert hex to struct', function() {
+    Input.readFrom(BufferReader(iwantHex)).should.deep.equal(issuance)
+  });
+
+  it('SpendInput: Test convert struct to hex', function() {
+    spend.writeTo().concat().toString('hex').should.equal(swantHex)
+  });
+
+  it('SpendInput: Test convert hex to struct', function() {
+    Input.readFrom(BufferReader(swantHex)).should.deep.equal(spend)
+  });
+
+  it('CoinbaseInput: Test convert struct to hex', function() {
+    coinbase.writeTo().concat().toString('hex').should.equal(cwantHex)
+  });
+
+  it('CoinbaseInput: Test convert hex to struct', function() {
+    Input.readFrom(BufferReader(cwantHex)).should.deep.equal(coinbase)
+  });
+
+//Todo: readFrom
+});
---
+++ test/map.js
@@ -0,0 +1,119 @@
+let Transaction = require('../lib/transaction/transaction.js')
+let Input = require('../lib/transaction/input.js')
+const Output = require('../lib/transaction/output.js')
+const BufferReader = require('../lib/binary/reader.js')
+let {mapTx} = require('../lib/transaction/map.js')
+const  BTMAssetID  = require('../lib/util/constance').BTMAssetID
+let bcIssuance = require('../lib/bc/issurance.js')
+let bcSpend = require('../lib/bc/spend.js')
+let bcCoinbase = require('../lib/bc/coinbase.js')
+
+let _ = require('lodash');
+
+const should = require('chai').should();
+const expect = require('chai').expect;
+let BN = require('bn.js')
+
+
+describe('Map', function() {
+
+  let testcase =[
+    {
+      inputs: [
+        Input.newSpendInput(null, "fad5195a0c8e3b590b86a3c0a95e7529565888508aecca96e9aeda633002f409", BTMAssetID, 88, 3, Buffer.from([1])),
+      ],
+      outputs: [
+        Output.newTxOutput(BTMAssetID, 80, Buffer.from[1]),
+      ]
+    },
+    {
+      inputs: [
+        Input.newIssuanceInput(new Buffer("nonce",'hex'), 254354, new Buffer("issuanceProgram", 'hex'), [new Buffer("arguments1","hex"), new Buffer("arguments2", 'hex')], new Buffer("assetDefinition", "hex")),
+      ],
+      outputs: [
+        Output.newTxOutput(BTMAssetID, 80,Buffer.from[1]),
+      ],
+    },
+    {
+      inputs: [
+        Input.newIssuanceInput(new Buffer("nonce", 'hex'), 254354, new Buffer("issuanceProgram", 'hex'), [new Buffer("arguments1", 'hex'), new Buffer("arguments2", 'hex')], new Buffer("assetDefinition", 'hex')),
+        Input.newSpendInput(null, "db7b16ac737440d6e38559996ddabb207d7ce84fbd6f3bfd2525d234761dc863", BTMAssetID, 88, 3, Buffer.from[1]),
+      ],
+      outputs:[
+        Output.newTxOutput(BTMAssetID, 80, Buffer.from[1]),
+        Output.newTxOutput(BTMAssetID, 80, Buffer.from[1]),
+      ],
+    }
+  ]
+
+  let txData = {
+    inputs: [
+      Input.newCoinbaseInput(new Buffer("TestMapCoinbaseTx")),
+    ],
+    outputs: [
+      Output.newTxOutput(BTMAssetID, 800000000000, Buffer.from[1]),
+    ]
+  }
+  it('TestMapSpendTx', function () {
+    for(let txData of testcase){
+      let tx = mapTx(txData)
+      let resultIds = tx.entries[_.findKey(tx.entries, 'resultIDs')].resultIDs
+      resultIds.length.should.equal(txData.outputs.length)
+
+
+      for(let i = 0; i< txData.inputs.length; i++) {
+        let oldIn = txData.inputs[i]
+        let resultEntry = tx.entries[tx.inputIDs[i]]
+
+        expect(resultEntry).not.to.be.undefined;
+        if (resultEntry instanceof bcIssuance) {
+          expect(resultEntry.value.assetID ).to.equal(oldIn.assetID())
+          expect(resultEntry.value.amount).to.equal(oldIn.amount())
+        }
+        else if (resultEntry instanceof bcSpend) {
+          let spendOut = tx.output(resultEntry.spentOutputId);
+          expect(spendOut.source.value).to.equal(oldIn.assetAmount())
+        } else {
+          throw Error("unexpect input type")
+        }
+      }
+
+      for(let i = 0; i< txData.outputs.length; i++) {
+        let oldOut = txData.outputs[i]
+        let newOut =tx.entries[resultIds[i]];
+
+        expect(newOut).not.to.be.undefined;
+
+        expect(newOut.source.value).to.equal(oldOut.outputCommitment.assetAmount)
+        expect(newOut.controlProgram.vmVersion).to.deep.equal(new BN(1))
+        expect(newOut.controlProgram.code).to.equal(oldOut.controlProgram)
+
+      }
+    }
+  });
+
+  it('TestMapCoinbaseTx', function () {
+    let oldOut = txData.outputs[0]
+
+    let tx = mapTx(txData)
+    let resultIds = tx.entries[_.findKey(tx.entries, 'resultIDs')].resultIDs
+
+    expect(tx.inputIDs.length).to.equal(1)
+    expect(tx.spentOutputIDs.length).to.equal(0)
+    expect(tx.gasInputIDs.length).to.equal(1)
+    expect(resultIds.length).to.equal(1)
+
+    let newOut = tx.entries[resultIds[0]]
+    expect(newOut).not.to.be.undefined;
+    expect(newOut.source.value).to.equal(oldOut.outputCommitment.assetAmount)
+
+    let mux = tx.entries[newOut.source.ref]
+    expect(mux).not.to.be.undefined;
+    expect(mux.witnessDestinations[0].value).to.equal(newOut.source.value)
+
+    let coinbase = tx.entries[tx.inputIDs[0]]
+    expect(coinbase).not.to.be.undefined;
+    expect(coinbase.witnessDestination.value).to.equal(mux.sources[0].value)
+  });
+
+})
